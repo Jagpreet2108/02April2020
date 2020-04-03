1d827268e4704f9eae948757ac6b66cfc08920d3
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-03 10:49:17 +0530

    Initial Commit One

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _03April2020.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {          
+            return View();
+        }
+    }
+}
